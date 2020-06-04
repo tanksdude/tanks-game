468140a215536f47c279d4c25656f2f919360671
fdsa <14tanks999@gmail.com>
2020-06-03 20:54:07 -0700

    slightly refactor (consolidate?) diagnostics

--- tanks-game/diagnostics.h
+++ tanks-game/diagnostics.h
@@ -12,10 +12,8 @@ public:
 	static std::chrono::time_point<std::chrono::steady_clock> getTime();
 	static long double getDiff(std::chrono::time_point<std::chrono::steady_clock> start, std::chrono::time_point<std::chrono::steady_clock> end);
 
-	static void startTiming();
 	static void startTiming(std::string);
 	static void endTiming();
-	static void addName(std::string);
 	static void clearTimes(); //(and names)
 	static void printTimings(); //plain milliseconds
 	static void printPreciseTimings(); //microsecond accuracy
