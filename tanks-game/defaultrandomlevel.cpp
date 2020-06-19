7b28ec1b425fefafbcdcd695a8744ad731cc74e1
fdsa <14tanks999@gmail.com>
2020-06-18 19:46:00 -0700

    default random level modernized

--- tanks-game/defaultrandomlevel.cpp
+++ tanks-game/defaultrandomlevel.cpp
@@ -28,15 +28,33 @@ void DefaultRandomLevel::initialize() { //still needs a lot of work
 
 	std::string paras[3] = {std::to_string(GAME_WIDTH/2), std::to_string(GAME_HEIGHT/2), std::to_string(randFunc() * 2*PI)};
 	HazardManager::pushCircleHazard(HazardManager::getCircleHazardFactory("stationary turret")(3, paras));
+	
+	int powerNum = PowerupManager::getNumPowerTypes("random-vanilla");
+	std::string* possiblePowers = new std::string[powerNum];
+	for (int i = 0; i < powerNum; i++) {
+		possiblePowers[i] = PowerupManager::getPowerName("random-vanilla", i);
+	}
 
-	std::string possiblePowers[] = { "speed", "wallhack", "bounce", "multishot", "triple", "homing", "invincible", "big", "megadeath", "grenade" }; //10
+	//get stacking status of the powers
+	bool* canStack = new bool[powerNum];
+	for (int i = 0; i < powerNum; i++) {
+		Power* p = PowerupManager::getPowerFactory("random-vanilla", possiblePowers[i])();
+		std::vector<std::string> attributes = p->getPowerAttributes();
+		canStack[i] = (std::find(attributes.begin(), attributes.end(), "stack") != attributes.end());
+		delete p;
+	}
+
+	float weights[] = { 1.0f, 1.0f, .25f };
 	for (int i = 0; i < 4; i++) {
-		int count = randFunc()*2 + 1; //{1, 2}
-		std::string* randPowers = RandomLevel::getRandomPowersOld(count, true, possiblePowers, 10);
+		int count = weightedSelect<float>(weights, 3) + 1; //{1, 2, 3}
+		std::string* randPowers = RandomLevel::getRandomPowers(count, canStack, possiblePowers, powerNum);
 		PositionHolder pos = RandomLevel::getSymmetricPowerupPositions_Corners(i, GAME_WIDTH/2, GAME_HEIGHT/2, GAME_WIDTH/2-60, GAME_HEIGHT/2-16);
-		PowerupManager::pushPowerup(new PowerSquare(pos.x, pos.y, randPowers, count));
+		PowerupManager::pushPowerup(new PowerSquare(pos.x, pos.y, "random-vanilla", randPowers, count));
 		delete[] randPowers;
 	}
+
+	delete[] possiblePowers;
+	delete[] canStack;
 }
 
 Level* DefaultRandomLevel::factory() {
