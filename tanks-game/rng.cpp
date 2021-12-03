f0e95443d88950f187b84a0f1cb5fab04a0702c7
asdf <14tanks999@gmail.com>
2021-12-03 14:36:05 -0800

    use C++ random, properly

--- tanks-game/rng.cpp
+++ tanks-game/rng.cpp
@@ -1,27 +1,33 @@
 #include "rng.h"
-#include <limits>
+//#include <limits>
 #include <cstdlib> //pretty sure this is auto-included in C++
+#include <chrono>
 
-const int RNG::MAX_RAND_NUM = std::numeric_limits<int>::max();
-std::mt19937 RNG::generator;
-std::uniform_int_distribution<int> RNG::distribution;
+//const int RNG::MAX_RAND_NUM = std::numeric_limits<int>::max();
+std::default_random_engine RNG::generator;
+std::uniform_real_distribution<double> RNG::distribution;
+//std::uniform_int_distribution<int> RNG::distribution;
 
-//apparently, I don't know how to initialize a mt19937 based on the current time
-//do you need very large time differences to see seed differences? I really don't know
-//switching back to the C way just to have different seeds
+//protip: don't use a mt19937
 
 void RNG::Initialize(long long seed) {
-	//std::mt19937 generator(seed); //mersenne_twister_engine
-	//std::uniform_int_distribution<int> distribution(0, RNG::MAX_RAND_NUM);
-	srand(seed);
+	//srand(seed);
+	generator = std::default_random_engine(seed);
+	std::uniform_int_distribution<int> distribution(0, 1);
+}
+
+void RNG::Initialize() {
+	RNG::Initialize(std::chrono::high_resolution_clock::now().time_since_epoch().count());
 }
 
 double RNG::randFunc() {
 	//return double(distribution(generator)) / (double(RNG::MAX_RAND_NUM) + 1);
-	return double(rand()) / (double(RAND_MAX) + 1);
+	//return double(rand()) / (double(RAND_MAX) + 1);
+	return distribution(generator);
 }
 
 double RNG::randFunc2() {
 	//return double(distribution(generator)) / double(RNG::MAX_RAND_NUM);
-	return double(rand()) / double(RAND_MAX);
+	//return double(rand()) / double(RAND_MAX);
+	return distribution(generator); //not [0,1] but whatever, it's not that important
 }
--- tanks-game/rng.h
+++ tanks-game/rng.h
@@ -3,12 +3,14 @@
 
 class RNG {
 private:
-	static const int MAX_RAND_NUM;
-	static std::mt19937 generator;
-	static std::uniform_int_distribution<int> distribution;
+	//static const int MAX_RAND_NUM;
+	static std::default_random_engine generator;
+	static std::uniform_real_distribution<double> distribution;
+	//static std::uniform_int_distribution<int> distribution; //for [a,b] instead of [a,b), but it just doesn't matter
 
 public:
 	static void Initialize(long long seed);
+	static void Initialize();
 	static double randFunc(); //[0,1)
 	static double randFunc2(); //[0,1]
 };
