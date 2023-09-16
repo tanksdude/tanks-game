b32512b087946d1fe7445ab6748bb80ba0eaa6d7
fdsa <14tanks999@gmail.com>
2023-09-16 14:42:29 -0700

    update RNG

--- tanks-game/rng.cpp
+++ tanks-game/rng.cpp
@@ -1,19 +1,12 @@
 #include "rng.h"
-//#include <limits>
-#include <cstdlib> //pretty sure this is auto-included in C++
 #include <chrono>
 
-//const int RNG::MAX_RAND_NUM = std::numeric_limits<int>::max();
 std::default_random_engine RNG::generator;
-std::uniform_real_distribution<double> RNG::distribution;
-//std::uniform_int_distribution<int> RNG::distribution;
-
-//protip: don't use a mt19937
+std::uniform_real_distribution<double> RNG::distribution(0, 1);
 
 void RNG::Initialize(long long seed) {
-	//srand(seed);
 	generator = std::default_random_engine(seed);
-	std::uniform_int_distribution<int> distribution(0, 1);
+	//std::uniform_real_distribution<double> distribution(0, 1);
 }
 
 void RNG::Initialize() {
@@ -21,18 +14,8 @@ void RNG::Initialize() {
 }
 
 double RNG::randFunc() {
-	//return double(distribution(generator)) / (double(RNG::MAX_RAND_NUM) + 1);
-	//return double(rand()) / (double(RAND_MAX) + 1);
 	return distribution(generator);
 }
-
-/*
-double RNG::randFunc2() {
-	//return double(distribution(generator)) / double(RNG::MAX_RAND_NUM);
-	//return double(rand()) / double(RAND_MAX);
-	return distribution(generator); //not [0,1] but whatever, it's not that important
-}
-*/
 
 int RNG::randNumInRange(int min, int max) {
 	return int(RNG::randFunc() * (max - min)) + min;
--- tanks-game/rng.h
+++ tanks-game/rng.h
@@ -3,16 +3,17 @@
 
 class RNG {
 private:
-	//static const int MAX_RAND_NUM;
 	static std::default_random_engine generator;
 	static std::uniform_real_distribution<double> distribution;
-	//static std::uniform_int_distribution<int> distribution; //for [a,b] instead of [a,b), but it just doesn't matter
 
 public:
 	static void Initialize(long long seed);
 	static void Initialize();
 
-	static double randFunc(); //[0,1)
-	//static double randFunc2(); //[0,1]
-	static int randNumInRange(int min, int max); //[min, max)
+	[[nodiscard]] static double randFunc(); //[0,1)
+	[[nodiscard]] static int randNumInRange(int min, int max); //[min, max)
+
+private:
+	RNG() = delete;
+	RNG(const RNG&) = delete;
 };
